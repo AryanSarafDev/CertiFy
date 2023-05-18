0a0a762e128f2efffcbe66c25c33b45764bde620
AryanSarafDev <aryansarafdev@gmail.com>
2023-05-18 22:15:39 +0530

    fix

--- lib/screens/navigate.dart
+++ lib/screens/navigate.dart
@@ -5,6 +5,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:supaverify/screens/per_S.dart';
 
 import 'org_S.dart';
+
 class NavigateS extends StatefulWidget {
   const NavigateS({Key? key}) : super(key: key);
 
@@ -23,10 +24,8 @@ class _NavigateSState extends State<NavigateS> {
 
   Future<List> certData() async {
     final idd = supabase.auth.currentUser!.email;
-    final boolc = await supabase
-        .from('everyone')
-        .select('*')
-        .textSearch('email', idd!);
+    final boolc =
+        await supabase.from('everyone').select('*').textSearch('email', idd!);
     print(boolc);
     return boolc;
   }
@@ -34,32 +33,27 @@ class _NavigateSState extends State<NavigateS> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(future: certData(),
+      body: FutureBuilder(
+          future: certData(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
-            var stuff = snapshot.data[0];
-    switch (snapshot.connectionState) {
 
-      case ConnectionState.none:return Center(child: CircularProgressIndicator());
-      case ConnectionState.waiting:
-        return Center(child: CircularProgressIndicator());
-        break;
-      default:
-        if (snapshot.hasData) {
-          print("applee");
-          print(stuff);
-          var orga = stuff['isorg'];
-          var pers = stuff['isper'];
-          var veri = stuff['isver'];
-          if (orga)
-            return orgS();
-          else if (pers) return PerS();
-          return orgS();
-        }else
-          return Center(child: CircularProgressIndicator());
+                if (snapshot.hasData) {
+                  var stuff = snapshot.data[0];
+                  print("applee");
+                  print(stuff);
+                  var orga = stuff['isorg'];
+                  var pers = stuff['isper'];
+                  var veri = stuff['isver'];
+                  if (orga)
+                    return orgS();
+                  else if (pers) return PerS();
+                  return orgS();
+                } else
+                  return Center(child: CircularProgressIndicator());
 
 
-        return Center(child: CircularProgressIndicator());
-    } }),
+            }
+          ),
     );
   }
 }
--- lib/screens/per_S.dart
+++ lib/screens/per_S.dart
@@ -51,119 +51,121 @@ class _PerSState extends State<PerS> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Welcome!",
-                          style: TextStyle(
-                              color: primary, fontSize: 60, fontFamily: 'Markbold'),
-                        ),
-                        Text(
-                          "$nw",
-                          style: TextStyle(
-                              color: primary, fontSize: 60, fontFamily: 'Markbold'),
-                        ),
-                      ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Welcome!",
+                            style: TextStyle(
+                                color: primary, fontSize: 60, fontFamily: 'Markbold'),
+                          ),
+                          Text(
+                            "$nw",
+                            style: TextStyle(
+                                color: primary, fontSize: 60, fontFamily: 'Markbold'),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () async {
-                    await supabase.auth.signOut();
-                  },
-                  icon: Icon(Icons.logout),
-                  color: primary,
-                )
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-              child: Container(
-                height: 600,
-                decoration: BoxDecoration(
-                    color: primary, borderRadius: BorderRadius.circular(20)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Text(
-                            "Certificates Recieved",
-                            style: TextStyle(
-                                color: third, fontSize: 20, fontFamily: 'Markbold'),
+                  IconButton(
+                    onPressed: () async {
+                      await supabase.auth.signOut();
+                    },
+                    icon: Icon(Icons.logout),
+                    color: primary,
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+                child: Container(
+                  height: 600,
+                  decoration: BoxDecoration(
+                      color: primary, borderRadius: BorderRadius.circular(20)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Text(
+                              "Certificates Recieved",
+                              style: TextStyle(
+                                  color: third, fontSize: 20, fontFamily: 'Markbold'),
+                            ),
                           ),
-                        ),
-                        IconButton(onPressed:(){setState(() {
+                          IconButton(onPressed:(){setState(() {
 
-                        });} , icon: Icon(Icons.refresh_outlined),color: third,)
-                      ],
-                    ),
-                    FutureBuilder(future: certData(),builder: (BuildContext context, AsyncSnapshot snapshot){
-                      if(snapshot.hasError)
-                      {
-                        return Center(child: Text(snapshot.error.toString()));
-                      }
-
-                      if(snapshot.hasData)
-                      {
-                        if(snapshot.data.length== 0)
+                          });} , icon: Icon(Icons.refresh_outlined),color: third,)
+                        ],
+                      ),
+                      FutureBuilder(future: certData(),builder: (BuildContext context, AsyncSnapshot snapshot){
+                        if(snapshot.hasError)
                         {
-                          return Center(child: Text("Empty"),);
-
+                          return Center(child: Text(snapshot.error.toString()));
                         }
-                        return ListView.builder(scrollDirection: Axis.vertical,
-                            shrinkWrap: true,itemCount: snapshot.data.length,itemBuilder: (context, int index){
-                              var stuff = snapshot.data[index];
-                              var namee=stuff['certname'];
+
+                        if(snapshot.hasData)
+                        {
+                          if(snapshot.data.length== 0)
+                          {
+                            return Center(child: Text("Empty"),);
+
+                          }
+                          return ListView.builder(scrollDirection: Axis.vertical,
+                              shrinkWrap: true,itemCount: snapshot.data.length,itemBuilder: (context, int index){
+                                var stuff = snapshot.data[index];
+                                var namee=stuff['certname'];
 
 
-                              var timee = stuff['created_at'];
-                              var orgg = stuff['orgname'];
+                                var timee = stuff['created_at'];
+                                var orgg = stuff['orgname'];
 
 
 
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Container( decoration: BoxDecoration(
-                                    color: secondary, borderRadius: BorderRadius.circular(20)),
+                                return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container( decoration: BoxDecoration(
+                                      color: secondary, borderRadius: BorderRadius.circular(20)),
 
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: ListTile(
-                                      title:Text("$namee",style: TextStyle(color: primary),textAlign: TextAlign.start,),
-                                      subtitle: Column(crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Text("$orgg",style: TextStyle(color: primary),textAlign: TextAlign.start,),Text("$timee",style: TextStyle(color: primary),textAlign: TextAlign.start,),
-                                        ],
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: ListTile(
+                                        title:Text("$namee",style: TextStyle(color: primary),textAlign: TextAlign.start,),
+                                        subtitle: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text("$orgg",style: TextStyle(color: primary),textAlign: TextAlign.start,),Text("$timee",style: TextStyle(color: primary),textAlign: TextAlign.start,),
+                                          ],
+                                        ),
+
+                                        trailing: IconButton(icon: Icon(Icons.download,color: primary,),onPressed:(){},),
                                       ),
-
-                                      trailing: IconButton(icon: Icon(Icons.download,color: primary,),onPressed:(){},),
                                     ),
                                   ),
-                                ),
-                              );
+                                );
 
-                            });
-                      }
-                      return Center(child: CircularProgressIndicator());
-                    },)
-                  ],
+                              });
+                        }
+                        return Center(child: CircularProgressIndicator());
+                      },)
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
